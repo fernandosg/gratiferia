ac97f26925787e57f287d9d3ca493f2e290ed1e8
Fernando Segura <fsg_tmp@hotmail.com>
2018-11-01 12:49:22 -0600

    HTML Structure for events

--- apps/events/models.py
+++ apps/events/models.py
@@ -8,7 +8,19 @@ class Event(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     address = models.TextField()
-    flyer = models.ImageField(upload_to="event_flyer", default='static/img/placeholder.gif')
+    flyer = models.ImageField(upload_to="event_flyer", default='/placeholder.gif')
 
     def last_event(self):
         return Event.objects.filter(date_event__gte=datetime.now()).first()
+
+    @property
+    def date_event_display(self):
+        return self.date_event.strftime("%d %b")
+
+    @property
+    def get_date(self):
+        return self.date_event.strftime("%d %M %y")
+
+    @property
+    def get_hour(self):
+        return self.date_event.strftime("%HH:%MM")
