ae4305b6549c9f04535e5d09b09c9917d1d82e67
Fernando Segura <fsg_tmp@hotmail.com>
2018-11-09 00:26:38 -0600

    Change verbose name

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,6 +3,6 @@ from custom_user.models import AbstractEmailUser
 
 # Create your models here.
 class User(AbstractEmailUser):
-    name = models.CharField(max_length=100)
-    first_last_name = models.CharField(max_length=100)
-    second_last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Nombre")
+    first_last_name = models.CharField(max_length=100, verbose_name="Apellido paterno")
+    second_last_name = models.CharField(max_length=100, verbose_name="Apellido materno")
